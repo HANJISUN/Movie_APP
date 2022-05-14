726bdd7e5b806e8828ca09303b4bd5dea532cbb5
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-05-15 02:46:12 +0900

    feat: 로컬스토리지 저장 구현

--- src/states/movie.ts
+++ src/states/movie.ts
@@ -1,5 +1,6 @@
 import { atom } from 'hooks/state'
 import { ISearchItem } from 'types/movie.d'
+import { AtomEffect } from 'recoil'
 
 const searchMovieList = atom<ISearchItem[]>({
   key: '#searchMovieList',
@@ -16,9 +17,23 @@ const searchMovieKeyword = atom<string>({
   default: '',
 })
 
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue))
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue))
+    })
+  }
+
 const favoriteMovieList = atom<ISearchItem[]>({
   key: '#favoriteMovieList',
   default: [],
+  effects: [localStorageEffect<ISearchItem[]>('GripBookMark')],
 })
 
 export { searchMovieList, MovieListpageNumber, searchMovieKeyword, favoriteMovieList }
