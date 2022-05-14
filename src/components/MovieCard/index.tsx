030896256343c58eeefd8e1c43f12147c90f9631
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-05-15 02:45:49 +0900

    add: poster 이미지 없는 경우, 디폴트 이미지 추가

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -7,6 +7,7 @@ import { useRecoilState } from 'recoil'
 import styles from './MovieCard.module.scss'
 import cx from 'classnames'
 import { GrFavorite } from 'react-icons/gr'
+import defaultImg from 'assets/default.png'
 
 import FavoriteModal from 'components/FavoriteModal'
 
@@ -29,11 +30,11 @@ const MovieCard = ({ item }: Props) => {
     <div>
       <li className={styles.itemContainer}>
         <div className={styles.contentBox} role='button' tabIndex={0} onClick={handleMovieCardClick}>
-          <img src={item.Poster} alt='poster' />
+          {item.Poster === 'N/A' ? <img src={defaultImg} alt='defaultImg' /> : <img src={item.Poster} alt='poster' />}
           <div className={styles.textBox}>
             <span className={styles.titleText}>{item.Title}</span>
             <span>{item.Year}</span>
-            <span>{item.Type}</span>
+            <span>{item.Type.toUpperCase()}</span>
           </div>
           <div className={cx(styles.icon, { [styles.filledIcon]: isAdded === 'true' })}>
             <GrFavorite />
